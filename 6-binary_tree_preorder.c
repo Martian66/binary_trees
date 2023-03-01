4e4cf8cb506d7883fa968794d0262d1ad2ff7b97
Martian66 <kimmartin049@gmail.com>
2023-03-01 10:03:40 -0800

    6-binary_tree_preorder.c

--- 6-binary_tree_preorder.c
+++ 6-binary_tree_preorder.c
@@ -10,9 +10,12 @@ void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int))
 {
 	if (tree == NULL || func == NULL)
 	{
-	return;
+		return;
 	}
-	func(tree->n);
-	binary_tree_traverse_pre_order(tree->left, func);
-	binary_tree_traverse_pre_order(tree->right, func);
+	else
+	{
+		func(tree->n);
+		binary_tree_traverse_pre_order(tree->left, func);
+		binary_tree_traverse_pre_order(tree->right, func);
+	}
 }

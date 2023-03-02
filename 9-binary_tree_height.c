5e5a7ee4eea876ae5b73c14ff3e4b38ea79b7331
Martian66 <kimmartin049@gmail.com>
2023-03-02 13:19:11 -0800

    9-binary_tree_height.c

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -9,15 +9,20 @@
 
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-	size_t height_left, height_right;
+	size_t left_height = 0;
+	size_t right_height = 0;
 
 	if (tree == NULL)
 	{
 		return (0);
 	}
-
-	height_left = binary_tree_height(tree->left);
-	height_right = binary_tree_height(tree->right);
-
-	return (1 + (height_left > height_right ? height_left : height_right));
+	else
+	{
+		if (tree)
+		{
+			left_height = tree->left ? 1 + binary_tree_height(tree->left) : 0;
+			right_height = tree->right ? 1 + binary_tree_height(tree->right) : 0;
+		}
+		return ((left_height > right_height) ? left_height : right_height);
+	}
 }

fea2fb18a3727d2c01717fec7a877137de8cd92f
Martian66 <kimmartin049@gmail.com>
2023-03-02 13:27:11 -0800

    9-binary_tree_height.c

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -9,20 +9,13 @@
 
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-	size_t left_height = 0;
-	size_t right_height = 0;
+	if (tree)
+	{
+		size_t l = 0, r = 0;
 
-	if (tree == NULL)
-	{
-		return (0);
+		l = tree->left ? 1 + binary_tree_height(tree->left) : 0;
+		r = tree->right ? 1 + binary_tree_height(tree->right) : 0;
+		return ((l > r) ? l : r);
 	}
-	else
-	{
-		if (tree)
-		{
-			left_height = tree->left ? 1 + binary_tree_height(tree->left) : 0;
-			right_height = tree->right ? 1 + binary_tree_height(tree->right) : 0;
-		}
-		return ((left_height > right_height) ? left_height : right_height);
-	}
+	return (0);
 }

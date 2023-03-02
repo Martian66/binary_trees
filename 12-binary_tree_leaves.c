e9968dd6c71fca7d2c5d7a361e21d737f02118d9
Martian66 <kimmartin049@gmail.com>
2023-03-02 03:01:03 -0800

    12-binary_tree_leaves.c

--- 12-binary_tree_leaves.c
+++ 12-binary_tree_leaves.c
@@ -10,10 +10,14 @@
 size_t binary_tree_leaves(const binary_tree_t *tree)
 {
 	if (tree == NULL)
+	{
 		return (0);
-
-	if (tree->left == NULL && tree->right == NULL)
-		return (1);
-
-	return (binary_tree_leaves(tree->left) + binary_tree_leaves(tree_right));
+	}
+	else
+	{
+		left = binary_tree_leaves(tree->left);
+		right = binary_tree_leaves(right->right);
+		leaf = left + right;
+		return ((!left && right) ? leaf + 1 : leaf + 0);
+	}
 }

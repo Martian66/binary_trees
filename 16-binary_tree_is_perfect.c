ffdd1ef585dd296671a08cce3668db29a971f118
Martian66 <kimmartin049@gmail.com>
2023-03-02 12:28:30 -0800

    16-binary_tree_is_perfect.c

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -9,13 +9,48 @@
 
 int binary_tree_is_perfect(const binary_tree_t *tree)
 {
+	int l = 0, r = 0;
+
+	if (tree->left && tree->right)
+	{
+		l = 1 + tree_is_perfect(tree->left);
+		r = 1 + tree_is_perfect(tree->right);
+		if (r == l && r != 0 && l != 0)
+			return (r);
+		return (0);
+	}
+	else if (!tree->left && !tree->right)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
+
+/**
+ * binary_tree_is_perfect - is perfect or not a tree
+ *@tree: tree to check
+ *
+ * Return: 1 if it is or 0 if its not
+ */
+
+int binary_tree_is_perfect(const binary_tree_t *tree)
+{
+	int result = 0;
+
 	if (tree == NULL)
+	{
 		return (0);
-
-	if (binary_tree_height(tree->left) != binary_tree_height(tree->right))
+	}
+	else
+	{
+		result = tree_is_perfect(tree);
+		if (result != 0)
+		{
+			return (1);
+		}
 		return (0);
-
-	return ((tree->left == NULL && tree->right == NULL) ||
-			(binary_tree_is_perfect(tree->left) &&
-			 binary_tree_is_perfect(tree->right)));
+	}
 }

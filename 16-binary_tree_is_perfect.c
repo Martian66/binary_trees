2f1dd6d51517662b897a354a3b9483df07299e9a
Martian66 <kimmartin049@gmail.com>
2023-03-02 12:32:36 -0800

    16-binary_tree_is_perfect.c

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -1,13 +1,13 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_is_perfect - checks if a binary tree is perfect
+ * tree_is_perfect - checks if a binary tree is perfect
  *@tree: a pointer to the root node of the tree
  *
  * Return: 0
  */
 
-int binary_tree_is_perfect(const binary_tree_t *tree)
+int tree_is_perfect(const binary_tree_t *tree)
 {
 	int l = 0, r = 0;
 
